78bd462e7cd006d3b4765e8a7d34080fe0905205
Phillip Gray <pixel.ninjad@gmail.com>
2017-07-23 09:49:31 +1200

    display image name and width/height

--- content/content.library.php
+++ content/content.library.php
@@ -13,10 +13,6 @@
 
 			$this->Context->appendChild($h2);
 
-
-			// $files = glob(URL . '/workspace/uploads/*');
-			// var_dump($files);
-
 			$files = glob(DOCROOT . '/workspace/uploads/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 			$directories = glob(DOCROOT . '/workspace/uploads/*', GLOB_ONLYDIR);
 
@@ -31,14 +27,27 @@
 			foreach($files as $file) {
 				$image_dimensions = getimagesize($file);
 				$image_src = str_replace(DOCROOT, URL, $file);
+				$image_name = str_replace('-', ' ', explode('.', basename($file))[0]);
 
 				$container = new XMLElement('div', null, array('class' => 'image-preview'));
+				$figure = new XMLElement('figure', null, array('data-imagesrc' => $image_src));
+
+				$meta = new XMLElement('div');
+				$name = new XMLElement('p', '<strong>Name: </strong><span>' . $image_name . '</span>');
+				$width = new XMLElement('p', '<strong>Width: </strong><span>' . $image_dimensions[0] . 'px</span>');
+				$height = new XMLElement('p', '<strong>Height: </strong><span>' . $image_dimensions[1] . 'px</span>');
+
+				$meta->appendChild($name);
+				$meta->appendChild($width);
+				$meta->appendChild($height);
 
 				$image = new XMLElement('img', null, array('src' => $image_src));
 				$image->setAttribute('data-width', $image_dimensions[0]);
 				$image->setAttribute('data-height', $image_dimensions[1]);
 
-				$container->appendChild($image);
+				$figure->appendChild($image);
+				$container->appendChild($figure);
+				$container->appendChild($meta);
 				$fieldset->appendChild($container);
 			}
 
