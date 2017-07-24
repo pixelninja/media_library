cbf856cbca1aead10da2a38aceab6fd60977b45b
Phillip Gray <pixel.ninjad@gmail.com>
2017-07-24 20:44:53 +1200

    open a preview of an image

--- assets/media_library.backend.js
+++ assets/media_library.backend.js
@@ -1,8 +1,10 @@
 jQuery(window).load(function () {
+	/*
+	 *	Having a drop down is a bit of overkill, so let's make it a single clickable item
+	 */
 	var ml_menu_item = jQuery('#nav a[href$="/extension/media_library/library/"]'),
 		ml_menu_group = ml_menu_item.parents('li:last');
 
-	// kill the subnav
 	ml_menu_group.find('ul').remove();
 
 	ml_menu_group
@@ -14,24 +16,79 @@ jQuery(window).load(function () {
 		});
 
 	(function ($) {
+		// Make sure we only execute in the Library
 		if (driver !== 'library') return false;
 
+		/*
+		 *	Go forward or backwards a directory
+		 */
+		var base_url = Symphony.Context.get('root') + '/symphony/extension/media_library/library/?folder=';
+
+		// Go back a directory level
 		$('.directory-back').on('click', function () {
 			var self = $(this),
-				url = Symphony.Context.get('root') + '/symphony/extension/media_library/library/?folder=',
+				// The handle to append, which is the full folder path minus the last folder
 				handle = folder_path.replace(folder_path.substring(folder_path.lastIndexOf('/')), '');
 
-			window.location.href = url + handle
+			window.location.href = base_url + handle;
 		});
 
 		$('.directory-preview').on('click', function () {
 			var self = $(this),
-				handle = self.data('handle'),
-				url = Symphony.Context.get('root') + '/symphony/extension/media_library/library/?folder=';
+				// The handle of the folder
+				handle = self.data('handle');
 
+			// If there is an existing folder path, add it to the new handle
 			if (folder_path) handle = folder_path + '/' + handle;
 
-			window.location.href = url + handle
+			window.location.href = base_url + handle;
 		});
+
+		/*
+		 *	Open the image in a lightbox
+		 */
+		 $('.image-preview img').on('click', function () {
+ 			var self = $(this),
+				// Store the image data
+ 				src = self.attr('src'),
+				width = self.data('width'),
+				height = self.data('height'),
+				// Add the lightbox wrapper
+				lightbox = $('body').append('<div id="lightbox" />').find('#lightbox').hide().fadeIn(200),
+				// Append the image to the lightbox
+				image = lightbox.append('<img src="' + src + '" />').find('img'),
+				close = lightbox.append('<span class="close" />').find('.close');
+
+			// Original image width is greater than 90% of window, and less than 90% of window height
+			if (width > $(window).width() * 0.8 && height <= $(window).height() * 0.8) {
+				image.css({
+					width : $(window).width() * 0.8,
+					height : 'auto'
+				});
+			}
+			// Otherwise if the image is greater than 90% window height
+			else if (height > $(window).height() * 0.8) {
+				image.css({
+					width : 'auto',
+					height : $(window).height() * 0.8
+				});
+			}
+			else {
+				image.css({
+					width : width,
+					height : height
+				});
+			}
+
+			close.css({
+				top : image.offset().top - 40,
+				left : image.offset().left + image.outerWidth()
+			})
+
+			setTimeout(function () {
+                image.addClass('slide-down');
+            }, 150);
+
+ 		});
 	})(jQuery);
 });
